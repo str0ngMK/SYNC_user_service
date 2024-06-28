19ec4a767c2073fa6d2886d00dc5cb184b6e1a43
hwlee9905 <9905hyun@naver.com>
2024-06-29 03:36:09 +0900

    [Test]

---
+++ src/main/java/user/controller/TestController.java
@@ -0,0 +1,36 @@
+package user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/user/api/v1")
+@RequiredArgsConstructor
+@Slf4j
+public class TestController {
+    private final WebClient.Builder webClientBuilder;
+    int loadBalanceCount = 0;
+    // existing code...
+    @GetMapping("/test")
+    public Mono<ResponseEntity<String>> responseTest() {
+        loadBalanceCount += 1;
+        log.info("loadBalanceCount : " + loadBalanceCount);
+
+        String url = "http://150.136.153.235:31585/project/api/v1/test";
+        return webClientBuilder.build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(response -> {
+                    log.info("Response from external API: " + response);
+                    return ResponseEntity.ok(response + " from user service");
+                });
+    }
+}
