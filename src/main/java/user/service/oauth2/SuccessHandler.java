3c492b52dac13655c35fb56be423989c73958942
hwlee9905 <9905hyun@naver.com>
2024-07-08 07:34:00 +0900

    [Test]

--- src/main/java/user/service/oauth2/SuccessHandler.java
+++ src/main/java/user/service/oauth2/SuccessHandler.java
@@ -57,7 +57,7 @@ public class SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private ResponseCookie createCookie(String key, String value) {
         ResponseCookie cookie = ResponseCookie.from(key, value)
             .path("/")
-            .sameSite("None")
+            .sameSite("Lax")
             .httpOnly(false)
             .secure(false)
             .maxAge(30 * 60)
