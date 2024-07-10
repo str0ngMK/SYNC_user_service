79a91040df821d6f3f038bf931ed7059fdd8669b
hwlee9905 <9905hyun@naver.com>
2024-07-11 08:33:18 +0900

    [Test]

--- src/main/java/user/service/global/config/WebConfig.java
+++ src/main/java/user/service/global/config/WebConfig.java
@@ -1,10 +1,7 @@
 package user.service.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 import user.service.global.filter.LogInterceptor;
 import user.service.global.filter.LoginCheckInterceptor;
@@ -28,7 +25,19 @@ public class WebConfig implements WebMvcConfigurer {
                         "/css/**", "/*.ico", "/error"
                 );
     }
-    
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+    }
+
     @Override
     public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
