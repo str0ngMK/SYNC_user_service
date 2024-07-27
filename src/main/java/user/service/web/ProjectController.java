3db9567fe7f460bf3e4f75ab135d4ce64fb2dfec
hwlee9905 <9905hyun@naver.com>
2024-07-28 02:13:58 +0900

    [Fix]
    swagger description수정

--- src/main/java/user/service/web/ProjectController.java
+++ src/main/java/user/service/web/ProjectController.java
@@ -48,7 +48,7 @@ public class ProjectController {
     }
     @Operation(summary = "유저가 속해있는 프로젝트들의 ID를 가져오기 위한 API", description = "HOST = 150.136.153.235:30080")
     @GetMapping("/project/api/v2")
-    public ResponseMessage getProjectsByUserLoginId(@Parameter(description = "존재하지 않는 프로젝트 아이디 입력시 오류 발생") @RequestParam String userId) {
+    public ResponseMessage getProjectsByUserLoginId(@Parameter(description = "존재하지 않는 로그인 아이디 입력시 오류 발생") @RequestParam String userId) {
         Long userEntityId = userService.getUserEntityId(userId);
         List<Long> projectIds = memberService.getProjectIdsByUserId(userEntityId);
         return new ResponseMessage("해당 유저의 프로젝트 아이디 조회 완료", true, projectIds);
